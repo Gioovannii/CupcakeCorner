795123ab4e7fc100a93a54e892e8595bed206fd5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-03 11:31:53 +0800

    Set User file

---
+++ CupcakeCorner/User.swift
@@ -0,0 +1,39 @@
+//
+//  User.swift
+//  CupcakeCorner
+//
+//  Created by Giovanni Gaffé on 2021/11/3.
+//
+
+import SwiftUI
+
+class UserRepresentable: Codable, ObservableObject {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Giovanni Gaffé"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct User: View {
+    var body: some View {
+        Text("Hello world")
+            .padding()
+    }
+}
+
+struct User_Previews: PreviewProvider {
+    static var previews: some View {
+        User()
+    }
+}
