482a13f097dc24fb450def4f09377b30d9d788e1
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-03 11:28:34 +0800

    New file for response

---
+++ CupcakeCorner/Response.swift
@@ -0,0 +1,20 @@
+//
+//  Response.swift
+//  CupcakeCorner
+//
+//  Created by Giovanni Gaffé on 2021/11/3.
+//
+
+import SwiftUI
+
+struct Response: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Response_Previews: PreviewProvider {
+    static var previews: some View {
+        Response()
+    }
+}
