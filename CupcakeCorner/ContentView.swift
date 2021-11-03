1be5d314b27877ec190dbbd80a67ec049b76c0ae
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-03 10:59:05 +0800

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Giovanni Gaffé on 2021/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Giovanni Gaffé on 2021/11/3.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
