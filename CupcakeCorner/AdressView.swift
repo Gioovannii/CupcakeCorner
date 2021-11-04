f42e8105cc22980dd2cc074d52a9e4e1d8254dbb
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-04 11:12:13 +0800

    New file to describe cupcakes

---
+++ CupcakeCorner/AdressView.swift
@@ -0,0 +1,20 @@
+//
+//  AdressView.swift
+//  CupcakeCorner
+//
+//  Created by Giovanni Gaffé on 2021/11/4.
+//
+
+import SwiftUI
+
+struct AdressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdressView()
+    }
+}
