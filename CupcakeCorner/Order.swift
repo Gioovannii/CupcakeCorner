6db29c27adc822fbf9f38e00b6f99140a869c815
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-04 10:30:04 +0800

    Set an order model

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,23 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Giovanni Gaffé on 2021/11/4.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+    
+    @Published var order = true
+
+
+}
