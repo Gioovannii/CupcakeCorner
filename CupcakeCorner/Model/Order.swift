9334bbcaab60627b4d626b8bbffc7b6c26c503db
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-05 20:35:17 +0800

    encode then decode check if has valid address and cost computed property

--- CupcakeCorner/Model/Order.swift
+++ CupcakeCorner/Model/Order.swift
@@ -28,7 +28,66 @@ class Order: ObservableObject, Codable {
     @Published var extraFrosting = false
     @Published var addSprinkles = false
     
-    @Published var order = true
+    @Published var name = ""
+    @Published var streetAdress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAdress: Bool {
+        if name.isEmpty || streetAdress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+    
+    var cost: Double {
+        var cost = Double(quantity) * 2
+        cost += Double(type) / 2
+        
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+        
+        if addSprinkles {
+            cost += Double(quantity / 2)
+        }
+        
+        return cost
+    }
+    
+    init() { }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+        
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
 
+        
+        name = try container.decode(String.self, forKey: .name)
+        streetAdress = try container.decode(String.self, forKey: .streetAdress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+        
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
 
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAdress, forKey: .streetAdress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+
+    }
 }
