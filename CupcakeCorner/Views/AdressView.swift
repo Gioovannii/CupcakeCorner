0c925b4cee8de5024c41d593ce2594152e0ce8a6
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-05 20:37:20 +0800

    Set another section with a navigation link for checkout

--- CupcakeCorner/Views/AdressView.swift
+++ CupcakeCorner/Views/AdressView.swift
@@ -19,7 +19,16 @@ struct AdressView: View {
                 TextField("Zip", text: $order.zip)
             }
             
-            
+            Section {
+                NavigationLink(destination: CheckoutView(order: order)) {
+                    Text("Check out")
+                        .foregroundColor(Color.accentColor)
+                }
+            }
+            .disabled(order.hasValidAdress == false)
+
+        }
+        .navigationBarTitle("Delivery details", displayMode : .inline)
     }
 }
 
